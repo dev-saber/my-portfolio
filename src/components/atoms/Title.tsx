ebf0d78472092719a890d68f49c24cd3e8c75ee9
Sifeddine Eddarai <sifeddine.edr@gmail.com>
2025-07-17 17:45:21 +0100

    ReactNode as children type

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { type ReactNode } from "react";
 
 const Title: React.FC<{
-  children: string;
+  children: ReactNode;
 }> = ({ children }) => (
   <h1 className="text-3xl sm:text-4xl font-bold text-neutral-800 dark:text-white mb-6 text-center">
     {children}
