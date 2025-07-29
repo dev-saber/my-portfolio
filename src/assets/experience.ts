a33bc9449aa5624a0a5d47b58cbc7981bb5daf5a
Sifeddine Eddarai <sifeddine.edr@gmail.com>
2025-07-29 17:19:16 +0100

    intelcia internship

--- src/assets/experience.ts
+++ src/assets/experience.ts
@@ -8,7 +8,29 @@ const experiences: {
   technologies: string[];
 }[] = [
   {
-    title: "Backend Engineering Intern",
+    title: "Software Engineering Intern",
+    company: "Intelcia IT Solutions",
+    location: "Rabat",
+    period: "Jul 2024 - Aug 2024",
+    duration: "2 months",
+    responsibilities: [
+      "Conducted vulnerability assessment of continuing with an outdated Keycloak version.",
+      "Studied the infeasibility of migrating the project to the latest Laravel version due to Keycloak package incompatibility.",
+      "Designed the architecture of a Keycloak realm and clients to manage users, roles, and permissions.",
+      "Built the project from scratch using Symfony, Twig, Tailwind, and JavaScript.",
+      "Implemented Docker for deployment.",
+    ],
+    technologies: [
+      "PHP",
+      "Symfony",
+      "Keycloak",
+      "JavaScript",
+      "Tailwind",
+      "Docker",
+    ],
+  },
+  {
+    title: "Software Engineering Intern",
     company: "Poste Maroc (Barid Al Maghrib)",
     location: "Rabat",
     period: "Jul 2024",
